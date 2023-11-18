e101c52e7bb33accc8a9bfbbc854a53d2a066c73
Svet <mursvyats@gmail.com>
2023-11-19 03:21:03 +0500

    init setup

---
+++ tailwind-discord/src/app/page.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import SideBar from "@/app/components/UI/SideBar";
+
+export default function Home() {
+  return (
+      <div className="flex">
+        <SideBar/>
+      </div>
+  )
+}
